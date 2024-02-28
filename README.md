0c447acb799c15b1c8677c03d96a7b72f94f65fb
Leon Jacobs <leonja511@gmail.com>
2024-02-28 07:41:43 +0200

    (feat) initial commit

--- README.md
+++ README.md
@@ -1,2 +1,55 @@
 # pmc
-Poor Mans CA
+
+Poor Mans CA is a small bash script to get a local CA up and running, quickly.
+
+Used in testing mTLS solutions
+
+## usage
+
+There are two major features. Creating a CA, and adding client certificates quickly.
+
+### makeca
+
+Run the script with the `makeca` subcommand, fill in the questions, and have a CA configured in the `ca/` directory.
+
+```bash
+$ ./ca.sh makeca
+
+	-- poor mans ca --
+
+i| generating private key @ ca/ca.key
+i| creating x509 certificate @ ca/ca.crt. get ready to answer some questions!
+You are about to be asked to enter information that will be incorporated
+into your certificate request.
+What you are about to enter is what is called a Distinguished Name or a DN.
+There are quite a few fields but you can leave some blank
+For some fields there will be a default value,
+If you enter '.', the field will be left blank.
+-----
+Country Name (2 letter code) [AU]:ZA
+State or Province Name (full name) [Some-State]:PewVille
+Locality Name (eg, city) []:PewPew
+Organization Name (eg, company) [Internet Widgits Pty Ltd]:
+Organizational Unit Name (eg, section) []:
+Common Name (e.g. server FQDN or YOUR name) []:pew.local
+Email Address []:
+```
+
+### add
+
+Add a new client with the `add` subcommand.
+
+```bash
+$ ./ca.sh add leon
+
+	-- poor mans ca --
+
+i| using ca certificate @ ca/ca.crt
+i| generating client key @ ca/leon/leon.key...
+i| generating csr @ ca/leon/leon.csr
+i| generating crt @ ca/leon/leon.crt
+Certificate request self-signature ok
+i| generating pem @ ca/leon/leon.pem
+i| done
+```
+
---
+++ ca.sh
@@ -0,0 +1,114 @@
+#!/bin/bash
+
+# 2024, @leonjza
+
+set -e
+
+echo
+echo "	-- poor mans ca --"
+echo
+
+ca_dir=ca
+ca_key_file_path=$ca_dir/ca.key
+ca_x509_cert_path=$ca_dir/ca.crt
+
+function make_ca() {
+
+	# check if ca directory already exists
+	if [[ -d $ca_dir ]]; then
+		echo "e| ca directory $ca_dir already exists"
+		return 1
+	else
+		mkdir -p $ca_dir
+	fi
+
+	if [[ -f "$ca_key_file_path" ]]; then
+		echo "e| ca key file path already exists"
+		return 1
+	fi
+
+	echo "i| generating private key @ $ca_key_file_path"
+	openssl genrsa \
+		-out $ca_key_file_path 4096
+
+	echo "i| creating x509 certificate @ $ca_x509_cert_path. get ready to answer some questions!"
+	openssl req -x509 -new -nodes \
+		-key $ca_key_file_path \
+		-sha256 -days 1024 \
+		-out $ca_x509_cert_path
+}
+
+function add_client() {
+	local client_name="$1"
+
+	echo "i| using ca certificate @ $ca_x509_cert_path"
+	echo "i| ca certificate subject: $(openssl x509 -in $ca_x509_cert_path -noout -subject)"
+
+	client_dir=$ca_dir/$client_name
+	client_key_path=$client_dir/$client_name.key
+	client_csr_path=$client_dir/$client_name.csr
+	client_crt_path=$client_dir/$client_name.crt
+	client_pem_path=$client_dir/$client_name.pem
+
+	if [[ -d $client_dir ]]; then
+		echo "e| client directory $client_dir already exists"
+		return 1
+	else
+		mkdir -p $client_dir
+	fi
+
+	echo "i| generating client key @ $client_key_path..."
+	openssl genrsa \
+		-out $client_key_path 2048
+
+	existing_subject=$(openssl x509 \
+		-in $ca_x509_cert_path -noout -subject | \
+		sed \
+			-e 's/^subject=//' \
+			-e 's/^[ \t]*//' \
+			-e 's|, emailAddress=[^/]*||' \
+	)
+	new_subject="/"$(echo "$existing_subject" | \
+		sed -r "s/(CN=)([^,]+)/\1${client_name}.\2/" | \
+		sed -e 's/, /\//g' \
+	)
+
+	echo "i| generating csr @ $client_csr_path"
+	echo "i| generating csr for subject: $new_subject"
+	openssl req -new -sha256 -key $client_key_path \
+		-subj $new_subject \
+		-out $client_csr_path
+
+	echo "i| generating crt @ $client_crt_path"
+	openssl x509 -req -in $client_csr_path \
+		-CA $ca_x509_cert_path -CAkey $ca_key_file_path \
+		-CAcreateserial -out $client_crt_path \
+		-days 365 -sha256
+
+	echo "i| generating pem @ $client_pem_path"
+	cat $client_crt_path $client_key_path > $client_pem_path
+
+	echo "i| done"
+}
+
+if [ $# -eq 0 ]; then
+    echo "No arguments provided. Exiting."
+    exit 1
+fi
+
+case "$1" in
+	makeca)
+		make_ca
+		;;
+	add)
+		if [ $# -lt 2 ]; then
+            echo "Subcommand 'add' requires a name argument"
+            exit 1
+        fi
+		add_client "$2"
+		;;
+	*)
+		echo "Unknown command: $1"
+		exit 1
+		;;
+esac
